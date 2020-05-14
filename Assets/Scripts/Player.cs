b8e46df78f4f88d961af5eed3868b5a66afee9db
FrostRing <alexandre1.lefevre@epitech.eu>
2020-05-14 17:42:38 +0200

    [ADD] Player Movements + Browser movement + Sound & Music samples + Quick fix to fish sprite

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public SpriteRenderer[] Pinguins;
+
+    private int currentPinguin;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentPinguin = 3;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q) && currentPinguin > 1)
+        {
+            Pinguins[currentPinguin - 1].color = new Color (1f, 1f, 1f, 0.5f);
+            currentPinguin -= 1;
+            Pinguins[currentPinguin - 1].color = new Color (1f, 1f, 1f, 1f);
+        }
+        else if (Input.GetKeyDown(KeyCode.D) && currentPinguin < 5)
+        {
+            Pinguins[currentPinguin - 1].color = new Color (1f, 1f, 1f, 0.5f);
+            currentPinguin += 1;
+            Pinguins[currentPinguin - 1].color = new Color (1f, 1f, 1f, 1f);
+        }
+    }
+    void Controller(string command) {
+        if (command == "Left" && currentPinguin > 1)
+        {
+            Pinguins[currentPinguin - 1].color = new Color (1f, 1f, 1f, 0.5f);
+            currentPinguin -= 1;
+            Pinguins[currentPinguin - 1].color = new Color (1f, 1f, 1f, 1f);
+        }
+        else if (command == "Right" && currentPinguin < 5)
+        {
+            Pinguins[currentPinguin - 1].color = new Color (1f, 1f, 1f, 0.5f);
+            currentPinguin += 1;
+            Pinguins[currentPinguin - 1].color = new Color (1f, 1f, 1f, 1f);
+        }
+    }
+}
